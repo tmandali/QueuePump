f8fb926cf14ac9d1cfe821b1a4e9e77bd109483e
Timur MANDALI <tmandali@yahoo.com>
2017-01-20 15:51:54 +0300

    gwegw

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -1,66 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
 using System.Threading.Tasks;
-using System.Linq;
-using System.Diagnostics;
-using System.Threading;
 
 namespace QueueProcessor
 {
     class Program
-    {
-        IList<ConnectionStringSettings> hostlist;
-        CancellationTokenSource cancellationTokenSource;
-        Task[] hosts;
-
+    {       
         static void Main(string[] args)
         {
-            var prg = new Program();
-            AsyncMain(prg).GetAwaiter().GetResult();
+            var svc = new Service();
+            AsyncMain(svc).GetAwaiter().GetResult();
         }
 
-        async static Task AsyncMain(Program prg)
+        async static Task AsyncMain(Service svc)
         {
-            prg.Init().Start(Environment.ProcessorCount, TimeSpan.FromSeconds(10));            
+            svc.Init().Start(Environment.ProcessorCount, TimeSpan.FromSeconds(10));            
             Console.ReadLine();            
-            await prg.Stop();
-        }
-
-        Program Init()
-        {
-            cancellationTokenSource = new CancellationTokenSource();
-            hostlist = ReadHost(new List<ConnectionStringSettings>());
-            return this;
-        }
-   
-        void Start(int maxConcurrencyPerHost, TimeSpan retry)
-        {
-            hosts = hostlist.Select(h => new HostConnect(h).Starter(maxConcurrencyPerHost, retry, cancellationTokenSource.Token)).ToArray();
-            Trace.TraceInformation("Service started");
-        }
-
-        async Task Stop()
-        {
-            cancellationTokenSource.Cancel();
-            var timeout = Task.Delay(TimeSpan.FromSeconds(30));
-            var finaly = await Task.WhenAny(timeout, Task.WhenAll(hosts)).ConfigureAwait(false);
-
-            if (finaly == timeout)
-                Trace.TraceWarning("Service process timeout");
-
-            cancellationTokenSource.Dispose();
-        }
-
-        IList<ConnectionStringSettings> ReadHost(IList<ConnectionStringSettings> list)
-        {
-            foreach (ConnectionStringSettings connectionSetting in ConfigurationManager.ConnectionStrings)
-            {
-                if (connectionSetting.Name.Split('.')[0] != "QueueHost")
-                    continue;
-                list.Add(connectionSetting);
-            }
-            return list;
+            await svc.Stop();
         }
     }
 }
---
+++ ConsoleApplication5/TableQueue/Service.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueueProcessor
+{
+    internal class Service
+    {
+        IList<ConnectionStringSettings> hostlist;
+        CancellationTokenSource cancellationTokenSource;
+        Task[] hosts;
+
+        public Service Init()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            hostlist = ReadHost(new List<ConnectionStringSettings>());
+            return this;
+        }
+
+        public void Start(int maxConcurrencyPerHost, TimeSpan retry)
+        {
+            hosts = hostlist.Select(h => new HostConnect(h).Starter(maxConcurrencyPerHost, retry, cancellationTokenSource.Token)).ToArray();
+            Trace.TraceInformation("Service started");
+        }
+
+        public async Task Stop()
+        {
+            cancellationTokenSource.Cancel();
+            var timeout = Task.Delay(TimeSpan.FromSeconds(30));
+            var finaly = await Task.WhenAny(timeout, Task.WhenAll(hosts)).ConfigureAwait(false);
+
+            if (finaly == timeout)
+                Trace.TraceWarning("Service process timeout");
+
+            cancellationTokenSource.Dispose();
+        }
+
+        IList<ConnectionStringSettings> ReadHost(IList<ConnectionStringSettings> list)
+        {
+            foreach (ConnectionStringSettings connectionSetting in ConfigurationManager.ConnectionStrings)
+            {
+                if (connectionSetting.Name.Split('.')[0] != "QueueHost")
+                    continue;
+                list.Add(connectionSetting);
+            }
+            return list;
+        }
+    }
+}
